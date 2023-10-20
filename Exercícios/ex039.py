6f66468604d73b84f05bb65dbdcb5fdf6e4a921b
isacpimenta <132321630+isacpimenta@users.noreply.github.com>
2023-10-19 21:34:37 -0300

    Ajustes ex039.py

--- Exercícios/ex039.py
+++ Exercícios/ex039.py
@@ -3,34 +3,43 @@ from os import system as os
 from datetime import date as dt
 
 os('cls')
-ano_atual = dt.today().year
 
-ano_nascimento = int(input('Informe seu ano de nascimento: '))
+sexo = input('Informe seu sexo [M/F]: ')
+sexo = sexo.upper()
 sleep(1)
 os('cls')
 
-# Lógica
-idade = ano_atual - ano_nascimento
-resposta = 18 - idade
+def alistamento():
+    ano_nascimento = int(input('Informe seu ano de nascimento: '))
 
-if idade == 18:
-    print('Quem nasceu em {} tem {} anos em {}'.format(ano_nascimento,idade, ano_atual))
-    sleep(3)
-    os('cls')
-    print('Você tem que se alistar ' '\033[31m' 'IMEDIATAMENTE' '\033[0m' '.')
+    ano_atual = dt.today().year    
+    idade = ano_atual - ano_nascimento
+    resposta = 18 - idade
 
-elif idade < 18:
-    print('Quem nasceu em {} tem {} anos em {}'.format(ano_nascimento, idade, ano_atual))
-    sleep(3)
-    os('cls')
-    print('Ainda faltam ' '\033[31m' '{}' '\033[0m' ' anos para seu alistamento.'.format(resposta))
-    sleep(3)
-    print('Seu alistamento será em ' '\033[31m' '{}' '\033[0m' '.'.format(ano_atual + resposta))
+    if idade == 18:
+        print('Quem nasceu em {} tem {} anos em {}'.format(ano_nascimento,idade, ano_atual))
+        sleep(3)
+        os('cls')
+        print('Você tem que se alistar ' '\033[31m' 'IMEDIATAMENTE' '\033[0m' '.')
 
-elif idade > 18:
-    print('Quem nasceu em {} tem {} anos em {}'.format(ano_nascimento, idade, ano_atual))
-    sleep(3)
-    os('cls')
-    print('Você já deveria ter se alistado há ' '\033[31m' '{}' '\033[0m' ' anos.'.format(abs(resposta)))
-    sleep(3)
-    print('Seu alistamento foi em ' '\033[31m' '{}' '\033[0m' '.'.format(ano_atual - abs(resposta)))
+    elif idade < 18:
+        print('Quem nasceu em {} tem {} anos em {}'.format(ano_nascimento, idade, ano_atual))
+        sleep(3)
+        os('cls')
+        print('Ainda faltam ' '\033[31m' '{}' '\033[0m' ' anos para seu alistamento.'.format(resposta))
+        sleep(3)
+        print('Seu alistamento será em ' '\033[31m' '{}' '\033[0m' '.'.format(ano_atual + resposta))
+
+    elif idade > 18:
+        print('Quem nasceu em {} tem {} anos em {}'.format(ano_nascimento, idade, ano_atual))
+        sleep(3)
+        os('cls')
+        print('Você já deveria ter se alistado há ' '\033[31m' '{}' '\033[0m' ' anos.'.format(abs(resposta)))
+        sleep(3)
+        print('Seu alistamento foi em ' '\033[31m' '{}' '\033[0m' '.'.format(ano_atual - abs(resposta)))
+
+if sexo == 'M':
+    alistamento()
+else:
+    print('Você ' '\033[31m' 'não' '\033[0m' ' precisa fazer o alistamento obrigatório!')
+    exit
